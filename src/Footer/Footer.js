4eb759ab2fd3a4903af4cb9ec0383bb100cdd3e2
islo123 <43340489+islo123@users.noreply.github.com>
2022-01-11 10:15:01 +0200

    Update Footer.js
    Nimi pois

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,6 @@ const Footer = function (){
     return (
         <div className="info">
             <p>E-Mail: AZVAfootball@hotmail.com</p>
-            <p>Whatsapp: +358468119512</p>
             <p>Instagram: azva_footballconsulting</p>
         </div>
     )
